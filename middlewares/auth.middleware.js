b68dc83e21f088bc650e650ff8e78b89892061a4
l00tus <marioalexandruu04@gmail.com>
2024-09-04 02:11:20 +0300

    implement authorization middleware

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if(!token) {
+        res.status(401).send({ "error": "No token provided" });
+    }
+
+    try {
+        const decodedUser = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decodedUser;
+        next();
+    } catch (error) {
+        res.status(401).send({ "error": "Invalid token" });
+    }
+}
+
+module.exports = authMiddleware;
