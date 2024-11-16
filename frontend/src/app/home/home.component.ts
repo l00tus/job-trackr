74bf6dd4d7e3ef5115e9c85cac6a72caaacae1be
l00tus <marioalexandruu04@gmail.com>
2024-11-16 03:35:21 +0200

    add homepage for non-logged in users

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  isLoggedIn: boolean = false;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.isLoggedIn().subscribe(
+      response => {
+      if(response.authenticated) {
+        this.isLoggedIn = true;
+      } else {
+        this.isLoggedIn = false;
+      }
+    },
+  );
+  }
 }
