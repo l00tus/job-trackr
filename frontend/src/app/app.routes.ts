80c355836b1b70b146d66caa294205e30823f738
l00tus <marioalexandruu04@gmail.com>
2024-11-22 13:34:31 +0200

    add wildcard route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,4 +7,5 @@ export const routes: Routes = [
     { path: 'login', component: LoginComponent},
     { path: 'register', component: RegisterComponent},
     { path: 'home', component: HomeComponent},
+    { path: '**', redirectTo: '/home', pathMatch: 'full'},
 ];
