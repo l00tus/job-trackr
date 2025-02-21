90aa6ee4c9434a87cc7a162da7534e38afde0c0e
l00tus <marioalexandruu04@gmail.com>
2025-02-21 19:50:48 +0200

    implement column chart for status analytics

--- frontend/src/app/analytics/status-distribution-chart/status-distribution-chart.component.ts
+++ frontend/src/app/analytics/status-distribution-chart/status-distribution-chart.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, SimpleChanges } from '@angular/core';
+import { Component, Input, SimpleChanges, ɵbypassSanitizationTrustResourceUrl } from '@angular/core';
 import { ChartType, Column, GoogleChartsModule } from 'angular-google-charts';
 
 @Component({
@@ -12,9 +12,21 @@ import { ChartType, Column, GoogleChartsModule } from 'angular-google-charts';
 export class StatusDistributionChartComponent {
   @Input() type: ChartType = ChartType.PieChart;
   @Input() data!: (string | number)[][];
+  columnData!: (string | number)[][];
   hasData: boolean = false;
 
-  options = {
+  statusColorMap: { [key: string]: string } = {
+      "Applied": "#87CEEB",
+      "To Apply": "#FFB6C1",
+      "Screening": "#A388EE",
+      "Interview": "#FD9745",
+      "Offer": "#FFDC58",
+      "Accepted": "#A3E636",
+      "Rejected": "#FF6B6B",
+      "Ghosted": "#D3D3D3"
+  };
+
+  pieOptions = {
     title: "Application Status Overview",
     titleTextStyle: {
       fontSize: 16,
@@ -24,7 +36,7 @@ export class StatusDistributionChartComponent {
     width: 1250,
     height: 500,
     backgroundColor: "#8DB1E6",
-    colors: ["#FD9745", "#FFDC58", "#FF6B6B", "#A3E636", "#A388EE", "#FFB6C1", "#87CEEB", "#D3D3D3"],
+    colors: [] as string[],
     pieSliceTextStyle: {
       color: "black",
     },
@@ -36,25 +48,78 @@ export class StatusDistributionChartComponent {
         color: "black"
       }
     },
-  }
+  };
+
+  columnOptions = {
+    title: "Application Status Overview",
+    titleTextStyle: {
+      fontSize: 16,
+      fontWeight: 550,
+      color: "black"
+    },
+    width: 1250,
+    height: 500,
+    backgroundColor: "#8DB1E6",
+    colors: [] as string[],
+    legend: {
+      position: 'none'
+    },
+    isStacked: true,
+    hAxis: {
+      title: 'Status',
+      textStyle: {
+        fontSize: 16,
+        color: 'black',
+      }
+    },
+  };
 
   chart!: {
     type: ChartType;
     data: (string | number)[][];
-    column: Column[];
+    columns: Column[];
     options: {};
   }
   
   ngOnChanges(changes: SimpleChanges) {
-    if(changes['data'] && this.data.length > 1) {
-      this.chart = {
-        type: this.type,
-        data: this.data.slice(1),
-        column: ['Status', 'Count'],
-        options: this.options,
-      };
-  
-      this.hasData = this.chart.data.length > 0;
+    if((changes['data'] || changes['type']) && this.data.length > 0) {
+      if(this.type == 'PieChart') {
+        const colors = this.data.map(row => this.statusColorMap[row[0] as string]);
+        this.pieOptions.colors = colors;
+
+        this.chart = {
+          type: this.type,
+          data: this.data,
+          columns: ['Status', 'Count'],
+          options: this.pieOptions,
+        };
+
+      } else if (this.type == 'ColumnChart') {
+        const statuses = this.data.map(row => row[0] as string);
+        const columns: Column[] = ['Status', ...statuses];
+
+        this.columnData = this.data.map((row, rowIndex) => {
+          const rowData = new Array(statuses.length).fill(0);
+          rowData[rowIndex] = row[1];
+
+          return [row[0], ...rowData];
+        });
+
+        const colors = statuses.map(status => this.statusColorMap[status]);
+
+        this.columnOptions.colors = colors;
+
+        this.chart = {
+          type: this.type,
+          data: this.columnData,
+          columns: columns,
+          options: this.columnOptions,
+        };
+
+        console.log(columns)
+      }
+      
+      this.hasData = true;
     } else {
       this.hasData = false;
     }
